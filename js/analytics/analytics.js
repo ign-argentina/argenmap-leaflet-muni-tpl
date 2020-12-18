e2012a66a0bb0353f7b323c81ed1c94d7221bcf8
andreazomoza <aszomoza@gmail.com>
2020-12-18 13:26:48 -0300

    ga4

---
+++ js/analytics/analytics.js
@@ -0,0 +1,6 @@
+$.getScript("https://www.googletagmanager.com/gtag/js?id=G-YY8394DZ3Y", function( data, textStatus, jqxhr ) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YY8394DZ3Y');
+});
